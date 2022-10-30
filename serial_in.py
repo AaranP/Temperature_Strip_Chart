ee409395c69746045e8e67fb9bd558057ced2c4f
Ashborn-Jinwoo <66931430+Ashborn-Jinwoo@users.noreply.github.com>
2022-10-30 15:39:15 -0700

    Add files via upload

---
+++ serial_in.py
@@ -0,0 +1,17 @@
+import time
+import serial
+
+# configure the serial port
+ser = serial.Serial(
+ port='/dev/cu.usbserial-D308HR8X',
+ baudrate=115200,
+ parity=serial.PARITY_NONE,
+ stopbits=serial.STOPBITS_TWO,
+ bytesize=serial.EIGHTBITS
+)
+
+ser.isOpen()
+while 1 :
+ strin = ser.readline()
+ print (strin)
+
